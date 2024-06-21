f4066cf62d68c6b52d6266daffc568e923f5ff28
Satont <satontworldwide@gmail.com>
2024-06-21 21:41:02 +0300

    feat(frontend): change stream path

--- apps/frontend/src/components/player.vue
+++ apps/frontend/src/components/player.vue
@@ -3,5 +3,5 @@
 </script>
 
 <template>
-  <iframe src="http://78.46.90.174:8889/mystream?muted=false?autoplay=true" class="h-full w-full" scrolling="no" />
+  <iframe src="https://streamx.satont.dev/mystream?muted=false?autoplay=true" class="h-full w-full" scrolling="no" />
 </template>
