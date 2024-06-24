349e14ef82790c3cd54e6382b385545fd0cdd738
Satont <satontworldwide@gmail.com>
2024-06-24 23:15:52 +0300

    fix(frontend): use dvg for chat and player height's

--- apps/frontend/src/pages/stream.vue
+++ apps/frontend/src/pages/stream.vue
@@ -5,11 +5,11 @@ import Player from '@/components/player/player.vue'
 
 <template>
   <div class="h-full flex flex-col xl:flex-row overflow-y-hidden">
-    <main class="h-[30vh] xl:h-full xl:flex-1 overflow-y-auto w-full">
+    <main class="h-[30dvh] xl:h-full xl:flex-1 overflow-y-auto w-full">
       <Player class="w-full h-full" />
     </main>
 
-    <aside class="h-[70vh] w-full xl:h-full xl:w-96">
+    <aside class="h-[70dvh] w-full xl:h-full xl:w-96">
       <Chat />
     </aside>
   </div>
